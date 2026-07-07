39f1b89822476fbb9e0a5a9e7ae8c3823ae80359
Edward Mallia <edwardsmallia@gmail.com>
2026-07-07 01:07:33 +0100

    requirements

--- sonictest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Test connection to Sonic HPC Cluster by printing details about the system
-
-print(f"Current working directory: {os.getcwd()}")
-print(f"Processor count: {os.cpu_count()}")
-print(f"Memory info: {os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024 ** 3):.2f} GB")
-print(f"System platform: {os.name}")
-print(f"Platform: {os.uname()}")
