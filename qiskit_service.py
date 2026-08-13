fbab50287b4b4e479be2bf9fc47ff92558823bce
Edward Mallia <25233259@login4.compute>
2026-08-13 23:09:49 +0100

    fixes

--- qiskit_service.py
+++ qiskit_service.py
@@ -2,5 +2,5 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 service = QiskitRuntimeService(
     channel="ibm_quantum",
-    token="IBM_QUANTUM_API_TOKEN"
+    token="ApiKey-1588aef2-e240-44f2-af7c-f6b7c42206bb"
 )
